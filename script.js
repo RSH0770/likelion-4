fd9d3d6f00e06ed7a65d81ab7e17fa12bbf0f98e
DESKTOP-OMUF54L\류승현 <nex721@naver.com>
2025-04-11 19:49:01 +0900

    두번째 커밋

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ resetCount.addEventListener("click", () => {
   count = 0;
   countBtn.innerText = count;
 });
+
+const colorSelect = document.querySelector("#colorSelect");
+const colorBox = document.querySelector("#colorBox");
+
+colorBox.style.width = "100px";
+colorBox.style.height = "100px";
+colorBox.style.backgroundColor = "gray";
+colorBox.style.marginTop = "10px";
+
+colorSelect.addEventListener("change", (e) => {
+  console.log(e);
+  colorBox.style.backgroundColor = e.target.value;
+});
+
+const nameForm = document.querySelector("#nameForm");
+const nameInput = document.querySelector("#nameInput");
+const submitResult = document.querySelector("#submitResult");
+
+nameForm.addEventListener("submit", (e) => {
+  console.log(e);
+  e.preventDefault();
+  submitResult.innerText = `안녕하세요, ${nameInput.value}님!`;
+  // submitResult.innerText = `안녕하세요, ${e.target[0].value}님!`;
+});
